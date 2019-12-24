e1dea9f4c88ec4a62346be667011efe849a06311
Aaron Wood <aaronjwood@outlook.com>
2019-12-23 21:34:15 -0700

    exercise04

---
+++ Chapter03/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool userInput;
+                byte numberPressed;
+                do
+                {
+                    Console.WriteLine("Please enter a whole number between 0 and 255.");
+                    userInput = byte.TryParse(Console.ReadLine(), out numberPressed);
+                    if (userInput == false)
+                    {
+                        Console.WriteLine("Your input was not a whole number between 0 and 255.");
+                    }
+                }    while (userInput != true);
+                    if(userInput == true)
+                {
+                    Console.WriteLine("Thank you!");
+                }
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("The number you entered was either too large or too small.");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Please input a whole number between 0 and 255.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception!" +
+                    $"\nSource: {ex.Source}" +
+                    $"\nType: {ex.GetType()}" +
+                    $"\nMessage: {ex.Message}" +
+                    $"\nStack Trace: {ex.StackTrace}\n" +
+                    $"\nInner Exception: {ex.InnerException}");
+            }
+        }
+    }
+}
