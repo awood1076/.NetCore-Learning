b5982c69e4ebdbe79ab7870fe2d7a4063d809488
Aaron Wood <aaronjwood@outlook.com>
2019-12-26 16:58:37 -0700

    finish chpt4

--- Chapter04/Instrumenting/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Instrumenting/Program.cs
@@ -16,6 +16,21 @@ namespace Instrumenting
             
             Debug.WriteLine("Debug says, I am watching!");
             Trace.WriteLine("Trace says, I am watching!");
+
+            var ts = new TraceSwitch("PacketSwitch", "This switch is set via command line.");
+
+            if (args.Length > 0)
+            {
+                if (System.Enum.TryParse<TraceLevel>(args[0], ignoreCase: true, result: out TraceLevel level))
+                {
+                    ts.Level = level;
+                }
+            }
+
+            Trace.WriteLineIf(ts.TraceError, "Trace Error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace Warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace Info");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace Verbose");
         }
     }
 }
