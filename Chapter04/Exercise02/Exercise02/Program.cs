c2f55f3805623a3db355bf945a961d434f2fec04
Aaron Wood <aaronjwood@outlook.com>
2019-12-26 23:37:41 -0700

    finished siev finally after doing it totally wrong

--- Chapter04/Exercise02/Exercise02/Program.cs
+++ Chapter04/Exercise02/Exercise02/Program.cs
@@ -7,7 +7,7 @@ namespace Exercise02
     class Program
     {
         //come back to this; do sieve of eratosthenes https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-        static bool[] PrimeFactor(int userInput)
+        static List<int> PrimeFactor(int userInput)
         {
             //Create my array that will be userInput long, defaults to false.
             bool[] eratosthenes = new bool[userInput];
@@ -15,58 +15,39 @@ namespace Exercise02
             int j = 0;
             List<int> primes = new List<int>();
 
-            for (int i = 0; i < Math.Sqrt(userInput); i++)
+            for (int i = 0; i < eratosthenes.Length; i++)
             {
-                if (i < 1)
-                {
-                    Debug.WriteLine("Loop < 1");
-                    continue;
-                }
-                else if (i == 2)
-                {
-                    Debug.WriteLine("===LOOP 2===");
-                    Debug.WriteLine($"{i}^2");
-                    j = Convert.ToInt32(Math.Pow(i, 2));
-                    Debug.WriteLine($"{j}");
-                    Debug.WriteLine($"{i}");
-                    primes.Add((int)j);
-                    eratosthenes[j] = true;
-                    Debug.WriteLine("============");
-                }
-                else if (i == 3)
-                {
-                    Debug.WriteLine("===LOOP 3===");
-                    Debug.WriteLine($"{i}^2 + {i}");
-                    j = Convert.ToInt32(Math.Pow(i, 2)) + i;
-                    Debug.WriteLine($"{j}");
-                    Debug.WriteLine($"{i}");
-                    primes.Add((int)j);
-                    eratosthenes[j] = true;
-                    Debug.WriteLine("============");
-                }
-                else if ((bool)eratosthenes.GetValue(i) == false && i >= 4)
-                {
-                    Debug.WriteLine($"===LOOP {i}===");
-                    j = Convert.ToInt32(Math.Pow(i, 2)) + (i * i);
-                    Debug.WriteLine($"{j}");
-                    Debug.WriteLine($"{i}");
-                    primes.Add((int)j);
-                    eratosthenes[j] = true;
-                    Debug.WriteLine("============");
-                }
-                else
-                {
-                    Debug.WriteLine("OUTSIDE OF LOOP!");
-                    continue;
-                }
-
-                foreach(int q in primes)
-                {
-                    Debug.Write($"Primes: {q}, ");
-                }
-                Debug.WriteLine("");
+                eratosthenes[i] = true;
             }
-            return eratosthenes;
+
+            Debug.WriteLine($"userInput = {userInput}");
+
+            //Run this to run through all the non-primes. It runs through the below while y < userInput, then goes back up and starts over with x incremented by 1 until y = userInput, etc.
+            for (int x = 2; x < userInput; x++)
+            {
+                Debug.WriteLine($"x = {x}");
+                Debug.WriteLine($"y = x * 2");
+                for (int y = x * 2; y < userInput; y = y + x)
+                {
+                    Debug.WriteLine($"y = {y} + {x}");
+                    if (primes.Contains(y) == false)
+                    {
+                        primes.Add(y);
+                        Debug.WriteLine($"{y} added to list");
+                    }
+                    Debug.WriteLine($"Now y will equal {y} + {x} or y + x");
+                }
+            }
+            
+            // scroll through the list and see what numbers are on there between 2 and user input; if it does exist on the list, it is NOT a prime.
+            for (int i = 2; i < userInput; i++)
+            {
+                if (primes.Contains(i) == false)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            return primes;
         }
 
         static void CallPrimeFactor()
