b3ce76b96f2587f9186b9add77b3d3cb52757e5a
Aaron Wood <aaronjwood@outlook.com>
2020-02-13 21:54:41 -0700

    begin work on exercise 2, create the class and program.

---
+++ Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Packt.CS7
+{
+    public class Person : Object //Object is a C# keyword for System.Object. Every class inherits from System.Object, but this is how to explicitly inherit from a class. System.Object is the super class, Person is the derived or subclass
+    {
+        //these are fields
+        public string Name;
+        public DateTime DateOfBirth;
+    }
+}
---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+using Packt.CS7;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person();
+            p1.Name = "Bob Smith";
+            p1.DateOfBirth = new System.DateTime(1965, 12, 22);
+            WriteLine($"{p1.Name} was born on {p1.DateOfBirth:dddd, d MMMM yyyy}");
+        }
+    }
+}
